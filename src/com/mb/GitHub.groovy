72c8dbd6593c1c19063bedbe6ad9b5ed082cb3a2
Bedril <moussakatbedril@yahoo.com>
2022-01-10 00:40:37 +0400

    Update GitHub.groovy

--- src/com/mb/GitHub.groovy
+++ src/com/mb/GitHub.groovy
@@ -16,17 +16,16 @@ class GitHub implements Serializable{
         )]){
             script.sh 'git config user.email "jenkins@example.com"'
             script.sh 'git config user.name "jenkins"'
-            script.sh 'git config --global --unset http.proxy'
-            script.sh 'git config --global --unset https.proxy'
 
             script.sh 'git status'
             script.sh 'git branch'
             script.sh 'git config --list'
 
-            script.sh ('git remote set-url origin https://$USER:$PASS@github.com/moussbed/java-mvn-app.git')
+            //script.sh ('git remote set-url origin https://$USER:$PASS@github.com/moussbed/java-mvn-app.git')
             script.sh 'git add .'
             script.sh 'git commit -m "ci: version bump"'
-            script.sh 'git push origin HEAD:jenkins-jobs'
+            // script.sh 'git push origin HEAD:jenkins-jobs'
+            script.sh "git push https://${USER}:${PASS}@github.com/${USER}/java-mvn-app.git"
         }
     }
 }
