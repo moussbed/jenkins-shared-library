4b895ea55b2e2cbe4480cea857ec14fcf2c991c5
Bedril <moussakatbedril@yahoo.com>
2022-01-10 08:00:20 +0400

    Update GitHub.groovy

--- src/com/mb/GitHub.groovy
+++ src/com/mb/GitHub.groovy
@@ -17,7 +17,6 @@ class GitHub implements Serializable{
             script.sh 'git config user.email "jenkins@example.com"'
             script.sh 'git config user.name "jenkins"'
             script.sh 'git config --global --unset http.proxy'
-            script.sh 'git config --global http.proxy http://proxyuser:proxypwd@proxy.server.com:8080'
 
             script.sh 'git status'
             script.sh 'git branch'
